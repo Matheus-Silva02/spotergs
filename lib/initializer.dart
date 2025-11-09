552a2dbe8e5500cc846c983e3324461a315b24b0
Matheus-Silva02 <matheus-silva02@uergs.edu.br>
2025-11-09 14:39:56 -0300

    Add MyApp and MyHomePage classes for application structure

---
+++ lib/initializer.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:spotergs/modules/home/pages/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
